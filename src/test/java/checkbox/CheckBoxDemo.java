430042b0c11c05c1bc67b432c94a01a70005939e
vasim <vasimkap1980@gmail.com>
2026-01-29 15:13:41 -0500

    selenium new files

--- src/test/java/checkbox/CheckBoxDemo.java
+++ src/test/java/checkbox/CheckBoxDemo.java
@@ -35,6 +35,7 @@ public class CheckBoxDemo {
 		{
 			check.click();
 		}
+        driver.quit();
 	}
 
 }
--- src/test/java/listners_extend_report/Listener_demo.java
+++ src/test/java/listners_extend_report/Listener_demo.java
@@ -1,11 +1,50 @@
 package listners_extend_report;
 
-import java.net.http.WebSocket.Listener;
-import java.util.List;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 
-public class Listener_demo implements Listener
-{
-	// how to generate extent_report 
-	
-	
+public class Listener_demo implements ITestListener {
+    private static ExtentReports extent;
+    private static ThreadLocal<ExtentTest> test = new ThreadLocal<>();
+
+    @Override
+    public void onStart(ITestContext context) {
+        ExtentHtmlReporter html = new ExtentHtmlReporter("target/extent-report.html");
+        html.config().setTheme(Theme.STANDARD);
+        extent = new ExtentReports();
+        extent.attachReporter(html);
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+        extent.flush();
+    }
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        ExtentTest t = extent.createTest(result.getMethod().getMethodName());
+        test.set(t);
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        test.get().pass("Test passed");
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        test.get().fail(result.getThrowable());
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        test.get().skip(result.getThrowable());
+    }
+
+    // other ITestListener methods can be left empty
 }
